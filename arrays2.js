dce82f2eda61c6fe5f5229dcac98c2f978170825
MrHasak99 <hamad_alkhalaf_99@hotmail.com>
2024-11-04 19:43:16 +0300

    correction

--- arrays2.js
+++ arrays2.js
@@ -1,81 +1,73 @@
+let odd = [1, 2, 3, 4, 5];
+let notOdd = [1, 2, 3, 4];
 function isArrayLengthOdd(numbers) {
-  let i = 0;
-  while (i < numbers.length) {
-    i++;
-  }
-  if (i % 2) {
-    console.log("This Array is Odd");
+  let answerOdd;
+  if (numbers.length % 2) {
+    answerOdd = "This Array is Odd";
   } else {
-    console.log("This Array is Not Odd");
+    answerOdd = "This Array is Not Odd";
   }
+  return answerOdd;
 }
-isArrayLengthOdd([1, 2, 3, 4, 5]);
-isArrayLengthOdd([1, 2, 3, 4]);
+console.log(isArrayLengthOdd(odd));
+console.log(isArrayLengthOdd(notOdd));
 
+let even = [1, 2, 3, 4, 5, 6];
+let notEven = [1, 2, 3];
 function isArrayLengthEven(numbers) {
-  let i = 0;
-  while (i < numbers.length) {
-    i++;
-  }
-  if (i % 2) {
-    console.log("This Array is Not Even");
+  let answerEven;
+  if (numbers.length % 2) {
+    answerEven = "This Array is Not Even";
   } else {
-    console.log("This Array is Even");
+    answerEven = "This Array is Even";
   }
+  return answerEven;
 }
-isArrayLengthEven([1, 2, 3, 4, 5, 6]);
-isArrayLengthEven([1, 2, 3]);
+console.log(isArrayLengthEven(even));
+console.log(isArrayLengthEven(notEven));
 
+let array = ["Moudhi", "Meshary", "Fahad", "Anwar", "Salman", "Ayah", "Aziz"];
 function addLailatoArray(instructors) {
-  console.log(instructors);
   instructors.push("Laila");
-  console.log("Laila added successfully, printing new array now:");
-  console.log(instructors);
+  return instructors;
 }
-addLailatoArray([
-  "Moudhi",
-  "Meshary",
-  "Fahad",
-  "Anwar",
-  "Salman",
-  "Ayah",
-  "Aziz",
-]);
+console.log(addLailatoArray(array));
 
-function eliminateTeam(teams) {
-  console.log(teams);
-  teams.pop();
-  console.log("Team eliminated successfully, printing new array now:");
-  console.log(teams);
+let teams = ["Brazil", "Germany", "Italy"];
+function eliminateTeam(team) {
+  team.pop();
+  return team;
 }
-eliminateTeam(["Brazil", "Germany", "Italy"]);
+console.log(eliminateTeam(teams));
 
+let evenFruits = ["apple", "orange", "banana", "kiwi"];
+let oddFruits = ["apple", "orange", "banana", "kiwi", "blueberry"];
 function secondHalfOfArrayIfItIsEven(fruits) {
-  let i = 0;
-  while (i < fruits.length) {
-    i++;
-  }
-  half = i / 2;
-  if (i % 2) {
+  const half = fruits.length / 2;
+  if (fruits.length % 2) {
     while (fruits.length > 0) {
       fruits.pop();
     }
-    console.log(fruits);
   } else {
-    console.log(fruits.slice(half, i));
+    fruits = fruits.slice(half, fruits.length);
   }
+  return fruits;
 }
-secondHalfOfArrayIfItIsEven(["apple", "orange", "banana", "kiwi"]);
-secondHalfOfArrayIfItIsEven(["apple", "orange", "banana", "kiwi", "blueberry"]);
+console.log(secondHalfOfArrayIfItIsEven(evenFruits));
+console.log(secondHalfOfArrayIfItIsEven(oddFruits));
 
+let hi = "HI!!!!!!!!!!";
+let taylor = "Taylor Schwift!!!!!!!!!!!";
+let hello = "Hellooooo";
 function youGottaCalmDown(shout) {
   exclamation = shout.indexOf("!");
   if (exclamation > 0) {
-    console.log(shout.slice(0, exclamation + 1));
+    shout = shout.slice(0, exclamation + 1);
   } else {
-    console.log(shout);
+    shout;
   }
+  return shout;
 }
-youGottaCalmDown("HI!!!!!!!!!!");
-youGottaCalmDown("Taylor Schwift!!!!!!!!!!!");
-youGottaCalmDown("Hellooooo");
+console.log(youGottaCalmDown(hi));
+console.log(youGottaCalmDown(taylor));
+console.log(youGottaCalmDown(hello));
